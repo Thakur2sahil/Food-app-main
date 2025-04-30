1805ad081ad9d3be54eed26af1e70b7b12049351
Sahil Thakur <sahilthakur1691@gmail.com>
2025-04-30 16:14:01 +0530

    [ADD][New feature]

--- server/routes/UserRoute/userRoute.js
+++ server/routes/UserRoute/userRoute.js
@@ -3,6 +3,7 @@ import multer from "multer";
 import ProductCard from "../../controller/UserPanel/ProductCard/ProductCardController.js";
 import ProductAdd from "../../controller/UserPanel/ProductAdd/ProductAddController.js";
 import CartCount from "../../controller/UserPanel/CartCount/CartCount.js";
+import Cart from "../../controller/UserPanel/CartInfo/CartInfoCOntroller.js";
 
 const userRoute = Router();
 
@@ -19,5 +20,8 @@ const upload = multer({ storage });
 userRoute.get("/card", ProductCard);
 userRoute.post("/card-add", ProductAdd);
 userRoute.post("/cart-count", CartCount);
+userRoute.get("/cart-info/:id", Cart.CartInfo);
+userRoute.post("/cart/increment", Cart.prdAdd);
+userRoute.post("/cart/decrement", Cart.prdSub);
 
 export default userRoute;
